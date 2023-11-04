d4b698f62ba6db0858405409288ee32b08c81bd7
admin <gorod3012@gmail.com>
2023-11-04 21:55:06 +0800

    *fix

--- src/ui/screens/UsersScreen.tsx
+++ src/ui/screens/UsersScreen.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
+import {useAppSelector, useData} from '../../services/hooks';
+import {selectUser} from '../../redux/reducers/user';
+
 interface UsersScreenProps {}
 
 const UsersScreen = (props: UsersScreenProps) => {
+  const users = useData('https://dummyjson.com/users');
   return (
     <View style={styles.container}>
       <Text>UsersScreen</Text>
